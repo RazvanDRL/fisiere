0ab1c113aeebe4605c86cb05dd13eec7065e1eb8
RazvanDRL <60571589+RazvanDRL@users.noreply.github.com>
2021-11-26 17:01:16 +0200

    Update fisiere.cpp

--- fisiere.cpp
+++ fisiere.cpp
@@ -6,6 +6,9 @@ int main()
     string input;
     cout << "Enter the fstream file name:\n";
     cin >> input;
+    cout << "#include <bits/stdc++.h>\n";
+    cout << "using namespace std;\n";
+    cout << endl;
     cout << "ifstream fin(\"" << input << ".in\");\n";
     cout << "ofstream fout(\"" << input << ".out\");\n";
 }
