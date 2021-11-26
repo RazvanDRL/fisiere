591de28bbea6c980c032b38606a079b630570568
RazvanDRL <60571589+RazvanDRL@users.noreply.github.com>
2021-11-26 16:58:21 +0200

    Add files via upload

---
+++ fisiere.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string input;
+    cout << "Enter the fstream file name:\n";
+    cin >> input;
+    cout << "ifstream fin(\"" << input << ".in\");\n";
+    cout << "ofstream fout(\"" << input << ".out\");\n";
+}
